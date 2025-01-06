f46e24310bd7f10762c3a67a9011ca2edc4ae048
Bryce Griffin <griffin.b@husky.neu.edu>
2025-01-06 18:49:00 -0500

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -11,7 +11,8 @@ MIRYOKU_X(MOUSE,  "Mouse") \
 MIRYOKU_X(MEDIA,  "Media") \
 MIRYOKU_X(NUM,    "Num") \
 MIRYOKU_X(SYM,    "Sym") \
-MIRYOKU_X(FUN,    "Fun") 
+MIRYOKU_X(FUN,    "Fun") \
+MIRYOKU_X(GAME,   "Game")
 
 #define U_BASE   0
 #define U_EXTRA  1
