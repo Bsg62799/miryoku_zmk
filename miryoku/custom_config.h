8ea6aff801721ee55e2d1988590f78fca47f9342
Bryce Griffin <griffin.b@husky.neu.edu>
2025-01-06 18:02:58 -0500

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -43,7 +43,7 @@ U_NP,              U_NP,              &kp PGUP,           &kp SPACE,         &kp
      K20, K21, K22, K23, K24,                                                        K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,                                                        K35, K36, K37, N38, N39 \
 ) \
-&kp ESC  XXX  XXX  XXX  XXX  XXX  &tog U_GAME                                              XXX  XXX  XXX  XXX  XXX  XXX  XXX \
+&kp ESC  &kp 1  XXX  XXX  XXX  XXX  XXX                                             XXX  XXX  XXX  XXX  XXX  XXX  XXX \
 XXX  K00  K01  K02  K03  K04  XXX                                               XXX  K05  K06  K07  K08  K09  XXX \
 XXX  K10  K11  K12  K13  K14  XXX       XXX  XXX                 XXX  XXX       XXX  K15  K16  K17  K18  K19  XXX \
 XXX  K20  K21  K22  K23  K24       XXX  XXX              K25  K26  K27  K28  K29  XXX \
